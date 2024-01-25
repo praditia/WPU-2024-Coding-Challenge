d2cea7d3352028607f91c7388e76f7354a337f74
Praditia Ilham Fauzi <aditfauzi007@gmail.com>
2024-01-25 23:52:48 +0700

    first day & fifth challenge

---
+++ 5-reduce-grow.js
@@ -0,0 +1,13 @@
+// function grow(x) {
+//   let result = x[0];
+//   for (let i = 1; i < x.length; i++) {
+//     result *= x[i];
+//   }
+//   return result;
+// }
+
+const grow = (x) => x.reduce((acc, el) => acc * el, 1);
+
+console.log(grow([1, 2, 3]));
+console.log(grow([4, 1, 1, 1, 4]));
+console.log(grow([2, 2, 2, 2, 2, 2]));
