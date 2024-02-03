6035ca783ee7cea50b3282ec4418aa5ad4074cd9
Praditia Ilham Fauzi <aditfauzi007@gmail.com>
2024-02-03 10:10:16 +0700

    Challenge-13

---
+++ 13-Convert-NumStr.js
@@ -0,0 +1,10 @@
+/* Cara 1 */
+// const numberToString = (num) => String(num)
+
+/* Cara 2 */
+// const numberToString = (num) => "" + num
+
+/* Cara 3 */
+const numberToString = (num) => `${num}`;
+
+console.log(numberToString(123));
