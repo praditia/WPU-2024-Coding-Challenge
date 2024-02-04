dcfd3dd5c44e651db621da172df416a3d6c7c122
Praditia Ilham Fauzi <aditfauzi007@gmail.com>
2024-02-04 11:43:39 +0700

    Challenge-20

---
+++ 20-Convert-StrNum.js
@@ -0,0 +1,8 @@
+// WPU Coding Challenge 2024
+// 20/366
+// https://www.codewars.com/kata/544675c6f971f7399a000e79/train/javascript
+
+/* Cara 1 */
+// const stringToNumber = (str) => Number(str);
+
+console.log(stringToNumber('1234'));
