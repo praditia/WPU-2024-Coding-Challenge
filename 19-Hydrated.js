b94e02b6264f265915900a4109a06ceafbef145f
Praditia Ilham Fauzi <aditfauzi007@gmail.com>
2024-02-04 11:37:07 +0700

    Challenge-19

---
+++ 19-Hydrated.js
@@ -0,0 +1,13 @@
+// WPU Coding Challenge 2024
+// 19/366
+// https://www.codewars.com/kata/582cb0224e56e068d800003c/train/javascript
+
+function litres(time) {
+  return Math.floor(time * 0.5);
+}
+
+console.log(litres(2));
+console.log(litres(1.4));
+console.log(litres(12.3));
+console.log(litres(0));
+console.log(litres(1787));
