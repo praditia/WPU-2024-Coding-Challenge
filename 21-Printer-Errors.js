dafa30d575d2f56a3642cf83463cfbade3ca8e85
Praditia Ilham Fauzi <aditfauzi007@gmail.com>
2024-02-05 11:11:52 +0700

    Challenge-22

---
+++ 21-Printer-Errors.js
@@ -0,0 +1,21 @@
+// WPU Coding Challenge 2024
+// 20/366
+// https://www.codewars.com/kata/56541980fa08ab47a0000040/train/javascript
+
+/* Cara 1 */
+// function printerError(s) {
+//   let letterError = 0;
+//   for (let i = 0; i < s.length; i++) {
+//     if (!s[i].match(/[a-m]/)) {
+//       letterError++;
+//     }
+//   }
+//   return `${letterError}/${s.length}`;
+// }
+
+/* Cara 2 */
+const printerError = (s) => `${(s.match(/[n-z]/g) || []).length}/${s.length}`;
+
+console.log(
+  printerError('aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz')
+);
