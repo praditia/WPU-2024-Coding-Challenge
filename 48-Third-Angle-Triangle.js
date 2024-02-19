d4ef073e60aa537b32b907ba35348edf148286bb
Praditia Ilham Fauzi <aditfauzi007@gmail.com>
2024-02-19 19:11:48 +0700

    Challenge-48

---
+++ 48-Third-Angle-Triangle.js
@@ -0,0 +1,9 @@
+// WPU Coding Challenge 2024
+// 48/366
+// https://www.codewars.com/kata/5a023c426975981341000014/train/javascript
+
+function otherAngle(a, b) {
+  return 180 - a - b;
+}
+
+console.log(otherAngle(43, 78));
