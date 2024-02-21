09cecf742c9adc863ce585d87d2a9a97289610c4
Praditia Ilham Fauzi <aditfauzi007@gmail.com>
2024-02-21 21:07:32 +0700

    Challenge-57

---
+++ 57-Return-Negative.js
@@ -0,0 +1,9 @@
+// WPU Coding Challenge 2024
+// 57/366
+// https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
+
+function makeNegative(num) {
+  return num < 0 ? num : -num;
+}
+
+console.log(makeNegative(-9));
