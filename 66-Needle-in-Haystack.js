126860190297acb0020b947fcf539f6739783011
Praditia Ilham Fauzi <aditfauzi007@gmail.com>
2024-02-24 10:41:13 +0700

    Challenge-66

---
+++ 66-Needle-in-Haystack.js
@@ -0,0 +1,34 @@
+// WPU Coding Challenge 2024
+// 65/366
+// https://www.codewars.com/kata/56676e8fabd2d1ff3000000c/train/javascript
+
+/* Cara 1 */
+// function findNeedle(haystack) {
+//   let needleIndex = 0;
+//   for (let i = 0; i < haystack.length; i++) {
+//     if (haystack[i] === 'needle') {
+//       needleIndex += i;
+//     }
+//   }
+
+//   return `found the needle at position ${needleIndex}`;
+// }
+
+/* Cara 2 */
+const findNeedle = (haystack) =>
+  `found the needle at position ${haystack.indexOf('needle')}`;
+
+console.log(
+  findNeedle([
+    '3',
+    '123124234',
+    undefined,
+    'needle',
+    'world',
+    'hay',
+    2,
+    '3',
+    true,
+    false,
+  ])
+);
