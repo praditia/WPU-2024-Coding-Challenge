40bab3a0670841080d09f36985365417a61f8c80
Praditia Ilham Fauzi <aditfauzi007@gmail.com>
2024-04-05 21:03:23 +0700

    Challenge-81

---
+++ 81-Sum-Strings.js
@@ -0,0 +1,10 @@
+// WPU Coding Challenge 2024
+// 81/366
+// https://www.codewars.com/kata/5966e33c4e686b508700002d/train/javascript
+
+function sumStr(a, b) {
+  return String(+a + +b);
+}
+
+console.log(sumStr('4', '5'));
+console.log(sumStr('34', '5'));
