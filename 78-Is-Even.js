e5d44228bc250d0424acb6644ce19d4d204f82eb
Praditia Ilham Fauzi <aditfauzi007@gmail.com>
2024-04-05 19:06:00 +0700

    Challenge-78

---
+++ 78-Is-Even.js
@@ -0,0 +1,13 @@
+// WPU Coding Challenge 2024
+// 78/366
+// https://www.codewars.com/kata/555a67db74814aa4ee0001b5/train/javascript
+
+function testEven(n) {
+  return n % 2 === 0;
+}
+
+console.log(testEven(0));
+console.log(testEven(0.5));
+console.log(testEven(1));
+console.log(testEven(2));
+console.log(testEven(4));
