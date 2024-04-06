1623705993da3d0cbde87b2e81a74687854168d5
Praditia Ilham Fauzi <aditfauzi007@gmail.com>
2024-04-06 11:53:03 +0700

    Challenge-96

---
+++ 96-Without-5.js
@@ -0,0 +1,11 @@
+// WPU Coding Challenge 2024
+// 96/366
+// https://www.codewars.com/kata/59441520102eaa25260000bf/train/javascript
+
+/* Cara 1 */
+// function unusualFive() {
+//   return ['a', 'b', 'x', 't', 'd'].length;
+// }
+
+/* Cara 2 */
+const unusualFive = () => 'aaaaa'.length;
