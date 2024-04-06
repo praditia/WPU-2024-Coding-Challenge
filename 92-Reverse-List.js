66e2537714caed6939196afe7f323141459b456a
Praditia Ilham Fauzi <aditfauzi007@gmail.com>
2024-04-06 11:21:38 +0700

    Challenge-92

---
+++ 92-Reverse-List.js
@@ -0,0 +1,10 @@
+// WPU Coding Challenge 2024
+// 92/366
+// https://www.codewars.com/kata/53da6d8d112bd1a0dc00008b/train/javascript
+
+function reverseList(list) {
+  return list.reverse();
+}
+
+console.log(reverseList([1, 2, 3, 4]));
+console.log(reverseList([3, 1, 5, 4]));
