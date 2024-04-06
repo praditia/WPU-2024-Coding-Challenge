0cc1e1544378607432c90b070f4bfbf8cf922386
Praditia Ilham Fauzi <aditfauzi007@gmail.com>
2024-04-06 10:45:27 +0700

    Challenge-90

---
+++ 90-If-Else-Debug.js
@@ -0,0 +1,10 @@
+// WPU Coding Challenge 2024
+// 90/366
+// https://www.codewars.com/kata/57089707fe2d01529f00024a/train/javascript
+
+function checkAlive(health) {
+  return health > 0;
+}
+
+console.log(checkAlive(5));
+console.log(checkAlive(0));
